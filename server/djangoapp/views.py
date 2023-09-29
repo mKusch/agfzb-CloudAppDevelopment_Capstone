2ea684ffcd4fd14b716e9258c02278e149c30c3c
Martin Kusch <kuschmail@web.de>
2023-09-29 08:42:56 -0400

    first week

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -14,16 +14,14 @@ import json
 logger = logging.getLogger(__name__)
 
 
-# Create your views here.
+def about(request):
+    context = {}
+    return render(request, 'djangoapp/about.html', context)
 
 
-# Create an `about` view to render a static about page
-# def about(request):
-# ...
-
-
-# Create a `contact` view to return a static contact page
-#def contact(request):
+def contact(request):
+    context = {}
+    return render(request, 'djangoapp/contact.html', context)
 
 # Create a `login_request` view to handle sign in request
 # def login_request(request):
